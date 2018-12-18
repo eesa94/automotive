cbbd5aad771db3a12322b21dfc4d46fdae015272
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-12-18 16:52:30 +0000

    smooth scroll; paroller on hero

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  
+  // smooth scrolling
+  var $root = $('html, body');
+  $('.nav-link').click(function() {
+    var href = $.attr(this, 'href');
+    if (href != undefined && href != '#') {
+      $root.animate({
+        scrollTop: $(href).offset().top
+      }, 500, function () {
+        window.location.hash = href;
+      });
+    }
+    return false;
+  });
+});
