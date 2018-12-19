af9f1f760eaf1451a3172f48d8a9f1f665feb7cf
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-12-19 08:53:29 +0000

    nav changes on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,40 @@
 $(document).ready(function(){
 
+    // resize navbar on scroll
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 400 && $(window).width() > 991) {
+            $('nav').css('height', '60px');
+            $('.brand').css('font-size', '3rem');
+        } else {
+            $('nav').css('height', '80px');
+            $('.brand').css('font-size', '3.5rem');
+        }
+    });
+    
   
-  // smooth scrolling
-  var $root = $('html, body');
-  $('.nav-link').click(function() {
+    // smooth scrolling
+    var $root = $('html, body');
+    $('.nav-link').click(function() {
     var href = $.attr(this, 'href');
     if (href != undefined && href != '#') {
-      $root.animate({
+        $root.animate({
         scrollTop: $(href).offset().top
-      }, 500, function () {
+        }, 500, function () {
         window.location.hash = href;
-      });
+        });
     }
     return false;
-  });
+    });
 
-  $('.box').hover(
-      function() {
-        $(this).css('opacity', '0.6')
-      },
-      function() {
-        $(this).css('opacity', '1')
-      }
-  );
+
+    // box hover
+    $('.box').hover(
+        function() {
+            $(this).css('opacity', '0.6')
+        },
+        function() {
+            $(this).css('opacity', '1')
+        }
+    );
 
 });
