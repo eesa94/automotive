d3c54463375c78ce13b33ae6a2af56478aa3e4db
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-12-19 11:54:46 +0000

    about blurb; font classes; favicon

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
     // resize navbar on scroll
     $(window).scroll(function() {
         if ($(this).scrollTop() > 400 && $(window).width() > 991) {
-            $('nav').css('height', '60px').css('background-color', 'rgba(0, 0, 0, 1)');
-            $('.brand').css('font-size', '3rem');
+            $('nav').css('height', '65px').css('background-color', 'rgba(0, 0, 0, 1)');
             $('.inner-menu').css('max-width', '1050px');
+            $('.brand').css('font-size', '3rem');
+            $('.nav-link').css('font-size', '1.6rem');
         } else {
             $('nav').css('height', '80px').css('background-color', 'rgba(0, 0, 0, 0.60)');
-            $('.brand').css('font-size', '3.5rem');
             $('.inner-menu').css('max-width', '1000px');
+            $('.brand').css('font-size', '3.5rem');
+            $('.nav-link').css('font-size', '1.8rem');
         }
     });
     
@@ -17,7 +19,7 @@ $(document).ready(function(){
     // smooth scrolling
     var $root = $('html, body');
 
-    $('.nav-link').click(function() {
+    $('.nav-link, .brand').click(function() {
         var href = $.attr(this, 'href');
         if (href != undefined && href != '#') {
             $root.animate({
