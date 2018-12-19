810306922fa535340de86cb0f93094678b95b099
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-12-19 09:10:25 +0000

    nav scroll - max-width

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,30 @@ $(document).ready(function(){
     // resize navbar on scroll
     $(window).scroll(function() {
         if ($(this).scrollTop() > 400 && $(window).width() > 991) {
-            $('nav').css('height', '60px');
+            $('nav').css('height', '60px').css('background-color', 'rgba(0, 0, 0, 1)');
             $('.brand').css('font-size', '3rem');
+            $('.inner-menu').css('max-width', '1050px');
         } else {
-            $('nav').css('height', '80px');
+            $('nav').css('height', '80px').css('background-color', 'rgba(0, 0, 0, 0.60)');
             $('.brand').css('font-size', '3.5rem');
+            $('.inner-menu').css('max-width', '1000px');
         }
     });
     
   
     // smooth scrolling
     var $root = $('html, body');
+
     $('.nav-link').click(function() {
-    var href = $.attr(this, 'href');
-    if (href != undefined && href != '#') {
-        $root.animate({
-        scrollTop: $(href).offset().top
-        }, 500, function () {
-        window.location.hash = href;
-        });
-    }
-    return false;
+        var href = $.attr(this, 'href');
+        if (href != undefined && href != '#') {
+            $root.animate({
+            scrollTop: $(href).offset().top
+            }, 500, function () {
+            window.location.hash = href;
+            });
+        }
+        return false;
     });
 
 
